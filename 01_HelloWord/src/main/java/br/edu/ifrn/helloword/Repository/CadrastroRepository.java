1f7e0f8e8888c1218b3474414c044271124c8748
ander <ander@DESKTOP-2REG98G>
2022-02-05 16:30:43 -0300

    commit inicial

--- 01_HelloWord/src/main/java/br/edu/ifrn/helloword/Repository/CadrastroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifrn.helloword.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.ifrn.helloword.dominio.Cliente;
-
-public interface CadrastroRepository extends JpaRepository<Cliente, Integer> {
-
-	Cliente findByid(long id);
-}
